5cd4effe3c01d161a28835a8372951fbd2761b42
Mateus Paulart <mateusp23@rede.ulbra.br>
2021-06-25 23:06:09 -0300

    adicão de paddingBottom na listagem

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -133,6 +133,7 @@ export function Home (){
                     />
                 )}
                 ItemSeparatorComponent={ () => <ListDivider /> }
+                contentContainerStyle={{paddingBottom: 69}}
                 style={styles.matches}
                 showsVerticalScrollIndicator={false}
             />
