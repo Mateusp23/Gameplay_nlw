6b577a7186865a771a686f37ca592a6cbd247ef4
Mateus Paulart <mateusp23@rede.ulbra.br>
2021-06-27 11:56:40 -0300

    persistindo dados do usuario, mantendo ele logado direto p home

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,14 @@
-import React, { createContext, useContext, useState, ReactNode} from "react";
+import React,
+{ 
+    createContext,
+    useContext,
+    useState,
+    ReactNode,
+    useEffect
+} from "react";
 
 import * as AuthSession from 'expo-auth-session';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
     const { REDIRECT_URI } = process.env; 
@@ -11,6 +19,8 @@ import * as AuthSession from 'expo-auth-session';
 
 
 import { api } from "../services/api";
+import { COLLECTION_USERS } from "../configs/database";
+
 import { set } from "react-native-reanimated";
 
 type User = {
@@ -62,11 +72,14 @@ function AuthProvider({ children }: AuthProviderProps){
                 const firstName = userInfo.data.username.split(' ')[0];
                 userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
 
-                setUser({
+                const userData = {
                     ...userInfo.data,
                     firstName,
                     token: params.access_token,
-                });
+                }
+
+                await AsyncStorage.setItem( COLLECTION_USERS, JSON.stringify( userData ));
+                setUser( userData );
             } 
         } catch {
             throw new Error ('Não foi possivel autenticar');
@@ -74,6 +87,21 @@ function AuthProvider({ children }: AuthProviderProps){
             setLoading(false);
         }
     }
+
+    async function loadUserStorageData() {
+        const storage = await AsyncStorage.getItem(COLLECTION_USERS);
+
+        if(storage){
+            const userLogged = JSON.parse(storage) as User;
+            api.defaults.headers.authorization = `Bearer ${userLogged.token}`;
+
+            setUser(userLogged);
+        }
+    }
+
+    useEffect(() => {
+        loadUserStorageData();
+    },[])
 
     return (
         <AuthContext.Provider value={{
